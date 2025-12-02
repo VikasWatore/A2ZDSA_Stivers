1e602ff2e433318588f22d8327530ba66ea2fc90
VikasWatore <watorevikas@gmail.com>
2025-12-02 10:18:25 +0530

    02_FindSecondSmallest_And_SecondLargestElement

---
+++ src/main/java/Arrays/FindSecondSmallest_And_SecondLargestElement.java
@@ -0,0 +1,42 @@
+package Arrays;
+
+public class FindSecondSmallest_And_SecondLargestElement {
+    public static void main(String[] args) {
+        int[] arr={1,2,5,4,8,7};
+        int secondSmallest=secondSmallest(arr);
+        int secondLargest=secondLargest(arr);
+
+        System.out.println("SecondSmallest: "+secondSmallest+ " SecondLargest: "+secondLargest);
+
+    }
+    public static int secondLargest(int[] arr){
+        int largest=arr[0],secondlargest=-1;
+
+        for (int i = 1; i <arr.length ; i++) {
+            if(arr[i]>largest){
+                secondlargest=largest;
+                largest=arr[i];
+            }else if(arr[i]<largest && arr[i]>secondlargest){
+                secondlargest=arr[i];
+            }
+        }
+        return secondlargest;
+    }
+
+    public static int secondSmallest(int[] arr){
+
+        int smallest=arr[0];
+        int secSmallest=Integer.MAX_VALUE;
+        for (int i = 1; i <arr.length ; i++) {
+            if(arr[i]<smallest){
+                secSmallest=smallest;
+                smallest=arr[i];
+            }else if(arr[i]>smallest && arr[i]<secSmallest){
+                secSmallest=arr[i];
+            }
+
+        }
+        return secSmallest;
+    }
+
+}
