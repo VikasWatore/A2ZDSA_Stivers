2c654e53c519fefb755d23f907d6dfd03c7a8169
VikasWatore <watorevikas@gmail.com>
2025-12-09 11:08:01 +0530

    TwoSum

--- src/main/java/Arrays/MediumProblems/TwoSumCheck_Pair_With_Given_Sum_Exists_In_Array.java
+++ src/main/java/Arrays/MediumProblems/TwoSumCheck_Pair_With_Given_Sum_Exists_In_Array.java
@@ -16,16 +16,18 @@ public class TwoSumCheck_Pair_With_Given_Sum_Exists_In_Array {
         int[] ans2 = twoSumExistsWithIndex(arr, target);
         System.out.println(Arrays.toString(ans2));
 
-        int[] ans3=twoSum(arr,target);
-            System.out.println(Arrays.toString(ans3));
+        int[] ans3 = twoSum(arr, target);
+        System.out.println(Arrays.toString(ans3));
 
+        int[] ans4 = twoSumWithTwoPointer(arr, target);
+        System.out.println(Arrays.toString(ans4));
     }
 
 
     // Time Complexity: O(N²) because we use two nested loops to check every possible
     // pair of elements in the array, where N is the size of the array.
     // Space Complexity: O(1) as we use a constant amount of extra space regardless of input size.
-        // return yes/No
+    // return yes/No
     public static String twoSumExists(int[] arr, int target) {
         int n = arr.length;
         for (int i = 0; i < n; i++) {
@@ -38,7 +40,8 @@ public class TwoSumCheck_Pair_With_Given_Sum_Exists_In_Array {
         }
         return "No";
     }
-        // return index
+
+    // return index
     public static int[] twoSumExistsWithIndex(int[] arr, int target) {
         int n = arr.length;
         for (int i = 0; i < n; i++) {
@@ -54,17 +57,44 @@ public class TwoSumCheck_Pair_With_Given_Sum_Exists_In_Array {
 
     //optimize Solution with hash
 
-    public static int[] twoSum(int[] arr,int target){
-        Map<Integer,Integer> map=new HashMap<>();
+    public static int[] twoSum(int[] arr, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
-             int rem=target-arr[i];
-             if(map.containsKey(rem)){
-                return new int[]{map.get(rem),i};
-             }
-             map.put(arr[i],i);
+            int rem = target - arr[i];
+            if (map.containsKey(rem)) {
+                return new int[]{map.get(rem), i};
+            }
+            map.put(arr[i], i);
         }
         return new int[]{};
     }
 
+    //with two pointer appraoch
+    public static int[] twoSumWithTwoPointer(int[] arr, int target) {
+        int n = arr.length;
+
+        int[][] arrWithIndex = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arrWithIndex[i][0] = arr[i];
+            arrWithIndex[i][1] = i;
+        }
+
+        Arrays.sort(arrWithIndex, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            int sum = arrWithIndex[left][0] + arrWithIndex[right][0];
+            if (sum == target) {
+                return new int[]{arrWithIndex[left][1], arrWithIndex[right][1]};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
 
 }
