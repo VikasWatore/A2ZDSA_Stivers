675c9abfaeb7fe936578a260a657a479b8e13318
VikasWatore <watorevikas@gmail.com>
2026-01-27 11:34:38 +0530

    FindMiddleElementInALinkedList

---
+++ src/main/java/LinkedList/FindMiddleElementInALinkedList.java
@@ -0,0 +1,51 @@
+package main.java.LinkedList;
+
+public class FindMiddleElementInALinkedList {
+    static Node findMiddle(Node head){
+        if (head==null ||head.next==null){
+            return head;
+        }
+        Node temp=head;
+        int count=0;
+        while (temp!=null){
+            count++;
+            temp=temp.next;
+        }
+
+        int mid=count/2+1;
+        temp=head;
+        while (temp!=null){
+            mid=mid-1;
+            if (mid==0){
+                break;
+            }
+            temp=temp.next;
+        }
+        return temp;
+    }
+    public static Node findMiddleWithAlgorithm(Node head){
+        Node slow=head;
+        Node fast=head;
+
+        while (fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+
+        Node middleNode=findMiddle(head);
+
+        System.out.println(middleNode.data);
+
+        Node midNode=findMiddleWithAlgorithm(head);
+        System.out.println(midNode.data);
+    }
+}
